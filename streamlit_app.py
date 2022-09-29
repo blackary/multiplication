82e041f03142c3d796bfbda679c18d3243503a95
Zachary Blackwood <zachary@streamlit.io>
2022-09-29 15:42:28 -0400

    Show stats for today

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,6 @@ if not name:
 
 
 def get_nums(name: str, num: int) -> tuple[int, int]:
-    # Generate integer from name and num
     random.seed(name + str(num))
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
@@ -28,24 +27,50 @@ def get_nums(name: str, num: int) -> tuple[int, int]:
 
 db = database(name)
 
+DAILY_GOAL = 100
+
 db["current_num"] = db.get("current_num", 0)
 idx = db["current_num"]
 
-correct_today = 0
+correct_today = []
 for i in range(idx):
     if "correct_answer_on" in db[i]:
         correct_on = db[i]["correct_answer_on"].date()
         if correct_on == datetime.now().date():
-            correct_today += 1
+            correct_today.append(db[i])
 
-st.progress(min(correct_today, 100))
-st.write(f"Number correct today: {correct_today} out of 100")
+num_correct = len(correct_today)
+
+percent = int(min(num_correct / DAILY_GOAL * 100, 100))
+st.progress(percent)
+st.write(f"Number correct today: {num_correct} out of {DAILY_GOAL}")
+
+if num_correct in [DAILY_GOAL, DAILY_GOAL + 1]:
+    st.write("## You have completed your daily practice! (You can go on if you want)")
+    for i in range(5):
+        st.balloons()
+
+if num_correct >= DAILY_GOAL:
+    with st.expander("See how you did today", expanded=True):
+        incorrect_guesses = sum([len(c["incorrect_guesses"]) for c in correct_today])
+        problems_missed = [c for c in correct_today if len(c["incorrect_guesses"]) > 0]
+        total_time = (
+            correct_today[-1]["correct_answer_on"] - correct_today[0]["correct_answer_on"]
+        )
+        time_in_minutes = int(total_time.total_seconds() / 60)
+        st.write(f"Total number correct: **{num_correct}**")
+        st.write(f"Total wrong answers: **{incorrect_guesses}**")
+        st.write(f"Problems missed:")
+        for p in problems_missed:
+            f"* {p['num1']} x {p['num2']} = {p['num1'] * p['num2']} (guessed {', '.join(str(g) for g in p['incorrect_guesses'])})"
+        st.write(f"Total time: **{time_in_minutes} minutes**")
 
 num1, num2 = get_nums(name, idx)
 
 text_answer = st.text_input(f"{num1} x {num2} = ")
+submit = st.button("Submit")
 
-if not st.button("Submit guess"):
+if not text_answer and not submit:
     st.stop()
 
 try:
@@ -58,7 +83,7 @@ if idx not in db:
     db[idx] = {
         "num1": num1,
         "num2": num2,
-        "answer": answer,
+        "answer": num1 * num2,
         "incorrect_guesses": [],
         "correct_answer_on": None,
     }
