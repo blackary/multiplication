c5ab48542e93122f8234132f06f6ac97f3c3dac3
Zachary Blackwood <zblackwo@gmail.com>
2022-09-29 09:04:51 -0400

    Update deploy script

--- run_app.sh
+++ run_app.sh
@@ -6,8 +6,8 @@ if [ -f .streamlit/database.sqlite ]; then
 	echo "Database already exists, skipping restore"
 else
 	echo "No database found, restoring from replica if exists"
-    litestream restore -v -if-replica-exists -o .streamlit/database.sqlite s3://streamlit-zcb/st-database/db
-fi
+    litestream restore -v -if-replica-exists -o .streamlit/database.sqlite s3://streamlit-zcb/multiplication/db
+fie
 
 # Run litestream with streamlit as the subprocess.
 exec litestream replicate -exec "streamlit run --server.port 8080 --server.enableCORS false streamlit_app.py"
